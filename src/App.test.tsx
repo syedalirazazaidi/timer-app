b314197e94d9d756bf6cdfb3f3402a8fca965818
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2020-08-12 20:32:26 +0500

    start up

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,16 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { shallow } from "enzyme";
+import App from "./App";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// test("renders learn react link", () => {
+//   const { getByText } = render(<App />);
+//   const linkElement = getByText(/hello world/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+describe("App", () => {
+  it("should render a <div />", () => {
+    const container = shallow(<App />);
+    expect(container.find("div").length).toEqual(2);
+  });
 });
